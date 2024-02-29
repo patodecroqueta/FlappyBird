aec04154359844a591bc9545046b402965c16b11
patodecroqueta <patodecroqueta@gmail.com>
2024-02-29 20:44:30 +0100

    numeritos movilización

--- Assets/GameAssets/scripts/detectorcolumnas.cs
+++ Assets/GameAssets/scripts/detectorcolumnas.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class detectorcolumnas : MonoBehaviour
 {
+
+    public TMP_Text textObject;
     public int puntuacion = 0;
     public int puntosPorColumna = 1;
     
     void OnTriggerExit(){
         puntuacion=puntuacion+puntosPorColumna;
+        textObject.GetComponentInChildren<TextMeshProUGUI>().SetText(puntuacion.ToString());
     }
 }
